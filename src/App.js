dea1848042a18ff40b711f20ef4cbc5c9edc0283
VedantHanda771 <vedanthanda123@gmail.com>
2024-04-30 22:04:28 +0530

    main

--- src/App.js
+++ src/App.js
@@ -1,25 +1,459 @@
-import logo from './logo.svg';
+import Home from './pages/home';
 import './App.css';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <>
+  <title>Your site's title should be here</title>
+  <meta charSet="UTF-8" />
+  <meta name="description" content="Your site's description should be here" />
+  <meta name="keywords" content="Your site's keywords should be here" />
+  <meta name="viewport" content="width=device-width,initial-scale=1" />
+  <meta httpEquiv="X-UA-Compatible" content="IE=Edge" />
+  <link rel="stylesheet" href="style.css" />
+  {/*[if IE 6]>
+	<style type="text/css">
+		* html .group {
+			height: 1%;
+		}
+	</style>
+  <![endif]*/}
+  {/*[if IE 7]>
+	<style type="text/css">
+		*:first-child+html .group {
+			min-height: 1px;
+		}
+	</style>
+  <![endif]*/}
+  {/*[if lt IE 9]> 
+	 
+  <![endif]*/}
+  <link
+    rel="stylesheet"
+    href="https://fonts.googleapis.com/css?family=Audiowide:regular&subset=latin,latin-ext"
+  />
+  <link
+    rel="stylesheet"
+    href="https://fonts.googleapis.com/css?family=Montserrat:100,100italic,200,200italic,300,300italic,regular,italic,500,500italic,600,600italic,700,700italic,800,800italic,900,900italic&subset=cyrillic,cyrillic-ext,latin,latin-ext,vietnamese"
+  />
+  <div className="background">
+    <img
+      className="place-your-image-here-double-click-to-edit"
+      src="images/place_your_image_here_dou.png"
+      alt=""
+    />
+    <img
+      className="place-your-image-here-double-click-to-edit-2"
+      src="images/place_your_image_here_dou_2.png"
+      alt=""
+    />
+    <div className="design">
+      <div className="wrapper">
+        <img
+          className="vector-smart-object-double-click-to-edit"
+          src="images/vector_smart_object_doubl_5.png"
+          alt=""
+        />
+        <img
+          className="vector-smart-object-double-click-to-edit-2"
+          src="images/vector_smart_object_doubl_6.png"
+          alt=""
+        />
+        <img
+          className="vector-smart-object-double-click-to-edit-3"
+          src="images/vector_smart_object_doubl_7.png"
+          alt=""
+        />
+        <img
+          className="vector-smart-object-double-click-to-edit-4"
+          src="images/vector_smart_object_doubl_8.png"
+          alt=""
+        />
+        <div className="group-4">
+          <div className="card">
+            <div className="rounded-rectangle-holder">
+              <div className="button" />
+            </div>
+            <div className="rounded-rectangle-holder-2">
+              <div className="button-2" />
+            </div>
+            <div className="rounded-rectangle-holder-3">
+              <div className="button-3" />
+            </div>
+            <div className="image group">
+              <img
+                className="vector-smart-object-double-click-to-edit-5"
+                src="images/vector_smart_object_doubl_16.png"
+                alt=""
+              />
+              <img
+                className="vector-smart-object-double-click-to-edit-6"
+                src="images/vector_smart_object_doubl_15.png"
+                alt=""
+              />
+              <img
+                className="vector-smart-object-double-click-to-edit-7"
+                src="images/vector_smart_object_doubl_14.png"
+                alt=""
+              />
+            </div>
+          </div>
+          <div className="text">
+            <p className="title">SECURITY MODE TYPE</p>
+            <div className="subtitle group">
+              <p className="sub">TYPE A</p>
+              <p className="sub-2">TYPE B</p>
+              <p className="sub-3">TYPE C</p>
+            </div>
+            <div className="body-text group">
+              <div className="row-4 match-height group">
+                <div className="col-12">
+                  <p className="body-text-2">
+                    Lorem ipsum dolor sit amet,
+                    <br />
+                    consectetur adipiscing elit,
+                    <br />
+                    sed do eiusmod tempor incididunt
+                  </p>
+                  <p className="body-text-3">See More</p>
+                </div>
+                <div className="col-13">
+                  <p className="body-text-4">
+                    Lorem ipsum dolor sit amet,
+                    <br />
+                    consectetur adipiscing elit,
+                    <br />
+                    sed do eiusmod tempor incididunt
+                  </p>
+                  <p className="body-text-5">See More</p>
+                </div>
+                <div className="col-14">
+                  <p className="body-text-6">
+                    Lorem ipsum dolor sit amet,
+                    <br />
+                    consectetur adipiscing elit,
+                    <br />
+                    sed do eiusmod tempor incididunt
+                  </p>
+                  <p className="body-text-7">See More</p>
+                </div>
+              </div>
+              <p className="body-text-8">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="group-3">
+          <div className="row group">
+            <img className="shape-3" src="images/shape_3.png" alt="" />
+            <img className="shape-2" src="images/shape_2.png" alt="" />
+            <img className="shape-4" src="images/shape_4.png" alt="" />
+          </div>
+          <div className="text-2 match-height group">
+            <div className="col-5">
+              <p className="sub-4">SECURTY SYSTEM</p>
+              <p className="body-text-9">
+                Lorem ipsum dolor sit amet,
+                <br />
+                consectetur adipiscing elit,
+                <br />
+                sed do eiusmod tempor incididunt
+              </p>
+            </div>
+            <div className="col-6">
+              <p className="sub-5">SMART LAMP</p>
+              <p className="body-text-10">
+                Lorem ipsum dolor sit amet,
+                <br />
+                consectetur adipiscing elit,
+                <br />
+                sed do eiusmod tempor incididunt
+              </p>
+            </div>
+            <div className="col-7">
+              <p className="sub-6">CCTV CONTROL</p>
+              <p className="body-text-11">
+                Lorem ipsum dolor sit amet,
+                <br />
+                consectetur adipiscing elit,
+                <br />
+                sed do eiusmod tempor incididunt
+              </p>
+            </div>
+          </div>
+        </div>
+        <img
+          className="vector-smart-object-double-click-to-edit-8"
+          src="images/vector_smart_object_doubl_17.png"
+          alt=""
+        />
+        <p className="body-text-12">
+          “Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.”
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p className="subtitle-2">Security System</p>
+        <p className="title-2">SMART HOME</p>
+      </div>
+      <div className="wrapper-2">
+        <img
+          className="vector-smart-object-double-click-to-edit-9"
+          src="images/vector_smart_object_doubl.png"
+          alt=""
+        />
+        <img
+          className="vector-smart-object-double-click-to-edit-10"
+          src="images/vector_smart_object_doubl_2.png"
+          alt=""
+        />
+        <img
+          className="vector-smart-object-double-click-to-edit-11"
+          src="images/vector_smart_object_doubl_3.png"
+          alt=""
+        />
+        <div className="col-18">
+          <p className="email">work@myemail.com</p>
+          <p className="address">Your Address Here</p>
+        </div>
+        <img
+          className="vector-smart-object-double-click-to-edit-12"
+          src="images/vector_smart_object_doubl_9.png"
+          alt=""
+        />
+        <img className="address-icon" src="images/address_icon.png" alt="" />
+        <img className="email-icon" src="images/email_icon.png" alt="" />
+        <img className="phone-icon" src="images/phone_icon.png" alt="" />
+        <p className="phone-number">+01 24 5678 91011</p>
+        <p className="body-text-13">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        <p className="headline">ABOUT US</p>
+        <div className="group-6 group">
+          <div className="col-2">
+            <div className="text-3">
+              <p className="title-3">
+                MONITOR
+                <br />
+                HOME
+                <br />
+                REMOTELY
+              </p>
+              <p className="body-text-14">
+                • Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                do eiusmod tempor <br />
+                <span className="text-style">&nbsp;</span>
+                <br />• incididunt ut labore et dolore magna aliqua. <br />
+                <span className="text-style">&nbsp;</span>
+                <br />• Ut enim ad minim veniam, quis nostrud exercitation
+                ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
+                aute irure dolor in
+                <br />
+                <span className="text-style">&nbsp;</span>
+                <br />• reprehenderit in voluptate velit esse cillum dolore eu
+                fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                proident, sunt in culpa qui officia deserunt mollit anim id est
+                laborum.
+              </p>
+              <p className="delivery">
+                Service
+                <br />
+                <span className="text-style-2">012345</span>
+              </p>
+              <p className="body-text-15">DOWNLOAD AVAILABLE ON</p>
+            </div>
+            <div className="design-2 group">
+              <div className="rectangle" />
+              <div className="rectangle-2" />
+            </div>
+          </div>
+          <div className="smartphone">
+            <div className="image-2">
+              <img
+                className="vector-smart-object-double-click-to-edit-13"
+                src="images/vector_smart_object_doubl_12.png"
+                alt=""
+              />
+              <p className="body-text-16">
+                FUTURE
+                <br />
+                IS NOW
+              </p>
+              <img
+                className="vector-smart-object-double-click-to-edit-14"
+                src="images/vector_smart_object_doubl_11.png"
+                alt=""
+              />
+              <div className="button-holder">Download Now</div>
+            </div>
+          </div>
+        </div>
+        <div className="group-5 group">
+          <img
+            className="vector-smart-object-double-click-to-edit-15"
+            src="images/vector_smart_object_doubl_13.png"
+            alt=""
+          />
+          <div className="col-3">
+            <div className="text-4">
+              <p className="body-text-18">SMART HOME</p>
+              <p className="body-text-19">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+              <p className="body-text-20">Read More</p>
+            </div>
+            <div className="button-4" />
+          </div>
+        </div>
+      </div>
     </div>
-  );
+    <div className="background-2">
+      <div className="text-5 match-height group">
+        <div className="col-4">
+          <p className="body-text-21">about</p>
+          <p className="body-text-22">
+            Lorem ipsum dolor sit amet, consec tetur adipisicing elit, sed do
+            eiusmod tempor incididunt ultimam quantum
+          </p>
+        </div>
+        <div className="col-11">
+          <p className="body-text-27">Service</p>
+          <p className="body-text-28">
+            Lorem ipsum
+            <br />
+            Lorem ipsum
+            <br />
+            Lorem ipsum
+            <br />
+            Lorem ipsum
+          </p>
+        </div>
+        <div className="col-10">
+          <p className="body-text-25">Contact</p>
+          <p className="body-text-26">
+            Lorem iptsum
+            <br />
+            Lorem ipsum
+            <br />
+            Lorem ipsum
+            <br />
+            Lorem ipsum
+          </p>
+        </div>
+        <div className="col-9">
+          <p className="body-text-23">Home</p>
+          <p className="body-text-24">
+            Lorem ipsum
+            <br />
+            Lorem ipsum
+            <br />
+            Lorem ipsum
+            <br />
+            Lorem ipsum
+          </p>
+        </div>
+      </div>
+      <div className="design-3" />
+    </div>
+    <div className="background-3">
+      <div className="text-6">Check what your house can do for you</div>
+      <div className="image-3 group">
+        <img
+          className="place-your-image-here"
+          src="images/place_your_image_here.jpg"
+          alt=""
+        />
+        <img
+          className="place-your-image-here1"
+          src="images/place_your_image_here1.jpg"
+          alt=""
+        />
+        <img
+          className="place-your-image-here2"
+          src="images/place_your_image_here2.jpg"
+          alt=""
+        />
+      </div>
+    </div>
+    <div className="group-1 group">
+      <div className="row-6 group">
+        <img
+          className="vector-smart-object-double-click-to-edit-16"
+          src="images/vector_smart_object_doubl_12.png"
+          alt=""
+        />
+        <nav className="nav">
+          <ul className="nav-list group">
+            <li>
+              <p className="nav-item-1">
+                <a href="#">Home</a>
+              </p>
+            </li>
+            <li>
+              <p className="nav-item-1-2">
+                <a href="#">About</a>
+              </p>
+            </li>
+            <li>
+              <p className="nav-item-1-3">
+                <a href="#">Access Control</a>
+              </p>
+            </li>
+          </ul>
+        </nav>
+        <div className="social-media">
+          <div className="icon group">
+            <img className="instagram" src="images/instagram.png" alt="" />
+            <img className="facebook" src="images/facebook.png" alt="" />
+            <img className="twitter" src="images/twitter.png" alt="" />
+            <img className="whatsapp" src="images/whatsapp.png" alt="" />
+          </div>
+        </div>
+        <div className="button-copy-holder">Search</div>
+      </div>
+      <div className="row-7 group">
+        <div className="col-19">
+          <p className="sub-7">
+            FUTURE
+            <br />
+            IS NOW
+          </p>
+          <p className="title-4">SMART HOME</p>
+          <p className="body-text-29">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod
+          </p>
+          <div className="button-holder-2">See More</div>
+        </div>
+        <img
+          className="vector-smart-object-double-click-to-edit-17"
+          src="images/vector_smart_object_doubl_18.png"
+          alt=""
+        />
+      </div>
+    </div>
+  </div>
+</>
+
+  )
 }
 
 export default App;
